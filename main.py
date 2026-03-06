a98d1e263565bc1f66c1de970630db79b52237bf
cyy-07 <3141925174a@gmail.com>
2026-03-05 21:07:41 -0800

    Day 5 forward selection complete with timer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 #calculate distance
 def Euclidean_distance(point1, point2):
     total = 0
@@ -25,16 +25,50 @@ def nn_classify(dataset, feature_set):
                 distance = Euclidean_distance(current_sample, dataset[j, feature_set])    #Important! Here we only calculate distance based on the selected features.
                 if distance < nearest_distance:
                     nearest_distance = distance
-                    nearest_neighbor_label = dataset[j, 0]  # Update the nearest neighbor's class label
+                    nearest_neighbor_label = dataset[j, 0]  # Update the nearest neighbor's class label.
         if label == nearest_neighbor_label:
             correct_classification_number += 1
     accuracy = correct_classification_number / len(dataset)
     return accuracy
 
 
-def forward_selection():
+def forward_selection(dataset, features_num):
+    start = time.time()
     current_feature_set = []
     the_best_overall_accuracy = 0
+    the_best_overall_feature_set = []
+    all_features = []                   #Here we initialize a list to store all the features, using a for loop to add all the features to this list. We will use this list to calculate the accuracy of using all features at the beginning of forward selection algorithm, and we will also use this list to make sure we are testing all the features in the dataset during the search process.
+    for f in range(1, features_num + 1):
+        all_features.append(f)
+    overall_accuracy = nn_classify(dataset, all_features)
+    print(f'Running nearest neighbor with all {features_num} features, using "leaving-one-out" evaluation, I get an accuracy of {overall_accuracy * 100:.1f}%')
+    print("Beginning search. The current feature set is empty.")
+    #For each level, we will add a new feature to the current feature set,
+    #and we compute each feature's accuracy. 
+    #Finally, we will pick the feature with the highest accuracy and add it to the current feature set.
+    for i in range (1,features_num+1):
+        best_accuracy_this_level = 0
+        best_feature_this_level = None
+        for j in range (1,features_num+1):      #note that we start from one because the feature index begins from 1!
+            if j not in current_feature_set:     #consider only the features that are not in the current feature set
+                current_feature_set.append(j)              #add the feature to the current feature set temporarily
+                accuracy = nn_classify(dataset, current_feature_set)
+                print(f"Using feature(s) {current_feature_set} accuracy is {accuracy:.2f}%")
+                current_feature_set.remove(j)              #debug: remove the feature from the current (temporary) feature set.
+                if accuracy > best_accuracy_this_level:    #if the accuracy is better than the best accuracy we have now, update the best accuracy. 
+                    best_feature_this_level = j
+                    best_accuracy_this_level = accuracy
+        #After we finish testing all the features that are not in the current feature set, we add the best feature of this level to current feature set.
+        current_feature_set.append(best_feature_this_level)  
+        print(f"Feature set {current_feature_set} was best, accuracy is {best_accuracy_this_level:.2f}%")
+        if best_accuracy_this_level > the_best_overall_accuracy:
+            the_best_overall_accuracy = best_accuracy_this_level
+            the_best_overall_feature_set = current_feature_set.copy()
+        else:
+            print("(Warning! At this level, the best accuracy is worse than the best overall accuracy! Continue search to see if we can find a better feature subset.(Avoiding local maximum!))")
+    print(f"Finished search! The best feature subset is {the_best_overall_feature_set}, which has an accuracy of {the_best_overall_accuracy:.2f}%")
+    print(f"Total search time: {time.time() - start:.1f} seconds")           #Here we calculate the total search time.
+
 
 def backward_elimination():
     
